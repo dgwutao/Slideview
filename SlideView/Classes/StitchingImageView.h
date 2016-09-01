58bbe52ed55eb172dc32d7a02b270dab8897ef52
Axel <dgwutao@gmail.com>
2016-09-01 11:31:04 +0800

    #update 20160901 11:30

---
+++ SlideView/Classes/StitchingImageView.h
@@ -0,0 +1,13 @@
+//
+//  StitchingImageView.h
+//  SlideView
+//
+//  Created by Ghost on 8/30/16.
+//  Copyright © 2016 axel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StitchingImageView : UIView
+
+@end
